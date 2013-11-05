c4603ee1e5700e7f8e299c4b63e11e3d2cfa5fa1
donJoomla <info@donjoomla.com>
2013-11-05 23:00:06 +0000

    docs

--- js/stuff.js
+++ js/stuff.js
@@ -25,10 +25,10 @@ $('#main').waypoint(function(direction) {
 $('#base').waypoint(function(direction) {
 	if(direction == 'down') {
 		$('footer').addClass('affix');
-		$('base').height(202);
+		$('base').css('margin-top', 201);
 	}
 	else if(direction == 'up') {
 		$('footer').removeClass('affix');
-		$('base').height(1);
+		$('base').css('margin-top', 1);
 	}
 }, { offset: 'bottom-in-view' });
