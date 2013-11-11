4dfd31dea344b78c907831dc608e2e56c7a437ad
abeMedia <adam@abemedia.co.uk>
2013-11-11 21:39:32 +0000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -27,3 +27,38 @@ if($(".demoFrame").length>0) {
 		$(element).iframeHeight();
 	}); */
 }
+
+// search autocomplete
+$('#search_control').typeahead({
+	prefetch: "{{ site.url }}/search.json",
+	remote: {
+		url: "http://clients1.google.com/complete/search?q=%QUERY&hl=en&client=partner&source=gcsc&partnerid={014812861817308790526:-rrfwxely2g}&ds=cse&nocache=" + Math.random().toString(),
+		dataType: 'jsonp',
+		filter: function (resp) {
+			return $.map(resp[1], function (item) {
+				return {
+					value: item,
+					tokens: item
+				};
+			});
+		}
+	}
+})
+.on('typeahead:selected', function(e){
+	e.target.form.submit();
+});
+
+// jQuery $_GET plugin
+(function($) {
+	$.QueryString = (function(a) {
+		if (a == "") return {};
+		var b = {};
+		for (var i = 0; i < a.length; ++i)
+		{
+			var p=a[i].split('=');
+			if (p.length != 2) continue;
+			b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+		}
+		return b;
+	})(window.location.search.substr(1).split('&'))
+})(jQuery);
