a297eaad6f933e22dee141ac5fb1bdb737a3ca41
Adam Bouqdib <adam@abemedia.co.uk>
2013-11-14 00:54:26 +0000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -96,31 +96,13 @@ function pushState(path) {
 $.getScript('//platform.twitter.com/widgets.js');
 
 
-// zopim chat 
-$.getScript('//v2.zopim.com/?14JibENISkzX2WdkicVa9c44GnlgSyGe', function() {
-    $zopim(function() {
-        $zopim.livechat.button.show();
-    });
-});
+// zopim chat
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='//v2.zopim.com/?14JibENISkzX2WdkicVa9c44GnlgSyGe';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
 
-/*
-window.$zopim||(function(d,s){
-    var z=$zopim=function(c){
-        z._.push(c)
-    },
-    $=z.s=d.createElement(s),
-    e=d.getElementsByTagName(s)[0];
-    z.set=function(o){
-        z.set._.push(o)
-    };
-    z._=[];
-    z.set._=[];
-    $.async=!0;
-    $.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?14JibENISkzX2WdkicVa9c44GnlgSyGe';
-    z.t=+new Date;
-    z.livechat.button.show();
-    $.type='text/javascript';
-    e.parentNode.insertBefore($,e)
-})(document,'script');
-*/
+$zopim(function() {
+    $zopim.livechat.button.show();
+});
