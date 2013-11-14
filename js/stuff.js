cd98834d788226c2cb782a6358fece7198d466ad
Adam Bouqdib <adam@abemedia.co.uk>
2013-11-14 00:03:51 +0000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -102,8 +102,10 @@ d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
 $.src='//v2.zopim.com/?14JibENISkzX2WdkicVa9c44GnlgSyGe';z.t=+new Date;$.
 type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
- $zopim(function() {
-    $zopim.livechat.badge.hide();
+
+  $zopim(function() {
+    $zopim.livechat.button.show();
   });
+
 
 
