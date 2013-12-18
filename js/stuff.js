16039fe940fbcb1b46a03835d4a58a0bef22d09c
abeMedia <info@abemedia.co.uk>
2013-12-18 14:39:44 +0000

    fix share popup width

--- js/stuff.js
+++ js/stuff.js
@@ -134,8 +134,8 @@ if ($(".js-share").length > 0) {
 		});
 		$(".js-share-btn a").click(function() {
 			var url = $(this).attr('href'),
-				width = 450,
-				height = 400,
+				width = 500,
+				height = 500,
 				left = (screen.width/2)-(width/2),
 				top = (screen.height/2)-(height/2);
 			newwindow=window.open(url, 'Share', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+width+', height='+height+', top='+top+', left='+left);
