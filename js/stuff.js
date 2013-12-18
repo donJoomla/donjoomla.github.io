0d84acbaf728990c0ad372e38fbcc372248072c4
abeMedia <info@abemedia.co.uk>
2013-12-18 03:19:12 +0000

    and again

--- js/stuff.js
+++ js/stuff.js
@@ -136,7 +136,7 @@ if ($(".js-share").length > 0) {
 			var url = $(this).attr('href'),
 				width = 300,
 				height = 400,
-				left = (screen.width/2)-(w/2),
+				left = (screen.width/2)-(width/2),
 				top = (screen.height/2)-(height/2);
 			newwindow=window.open(url, 'Share', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+width+', height='+height+', top='+top+', left='+left);
 			if (window.focus) {newwindow.focus()}
