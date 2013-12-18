fa6a9abd1b5ce0504dbe3ef7597e0b4b9701be7f
abeMedia <info@abemedia.co.uk>
2013-12-18 16:37:47 +0000

    Update URLs

--- js/stuff.js
+++ js/stuff.js
@@ -137,7 +137,7 @@ if ($(".js-share").length > 0) {
 				width = 500,
 				height = 500,
 				left = (screen.width/2)-(width/2),
-				top = (screen.height/2)-(height/2);
+				top = (screen.height/2)-(height/1.5);
 			newwindow=window.open(url, 'Share', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+width+', height='+height+', top='+top+', left='+left);
 			if (window.focus) {newwindow.focus()}
 			return false;
